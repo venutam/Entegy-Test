7168a1a2b16563da9d66c8b80fc0e8c43e0205b9
Venugopalan <Venugopalan@DESKTOP-NJ6RJ49>
2019-11-27 17:59:19 +1000

    Add project files.

---
+++ EntegyAPI/Controllers/ChequeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EntegyAPI.Interface;
+
+namespace EntegyAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ChequeController : ControllerBase
+    {
+        private readonly IChequeService _service;
+
+        public ChequeController(IChequeService service)
+        {
+            _service = service;
+        }
+
+        // GET api/v1/values/2.5
+        [HttpGet("{amt}")]
+        public ActionResult<string> Get(string amt)
+        {
+            var result = _service.ConvertDecimalToWord(amt);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ EntegyAPI/Services/ChequeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntegyAPI.Interface;
+
+namespace EntegyAPI.Services
+{
+    public class ChequeService : IChequeService
+    {
+        public string ConvertDecimalToWord(string amt)
+        {
+            return "Success";
+        }
+    }
+}
